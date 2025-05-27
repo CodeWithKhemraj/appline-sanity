82e924c644d4ae9bf0353902d9fd032c0157463d
khemraj <khemraj@rwit.io>
2025-05-27 12:52:58 +0530

    Call starter kit features section

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -6,9 +6,10 @@ import {headerType} from './global/header'
 import {footerType} from './global/footer'
 import Page from './document/page'
 import HeroSection from './blocks/heroSection'
+import StarterKitFeatures from './blocks/starterKitFeatures'
 
 export const schemaTypes = [productType, headerType, footerType, Page]
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, productType, headerType, footerType, Page, HeroSection],
+  types: [blockContentType, productType, headerType, footerType, Page, HeroSection, StarterKitFeatures],
 } 
